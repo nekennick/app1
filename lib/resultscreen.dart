9c358ab748cc6b689cf63efc2167d2981afbe8fb
Khoa <nekennick@gmail.com>
2023-07-01 23:05:58 +0700

    save

--- lib/resultscreen.dart
+++ lib/resultscreen.dart
@@ -59,7 +59,7 @@ class _ResultScreenState extends State<ResultScreen> {
             Text(' ${widget.name1} ', style: const TextStyle(fontSize: 32)),
             SizedBox(
               height: 40,
-              child: Image.asset('lib/icons/love.png'),
+              child: Image.asset('assets/icons/love.png'),
             ),
             Text(' ${widget.name2} ', style: const TextStyle(fontSize: 32)),
             Padding(
@@ -78,7 +78,7 @@ class _ResultScreenState extends State<ResultScreen> {
                       : Text(
                           widget.result,
                           style: const TextStyle(
-                              fontSize: 22, color: Colors.white),
+                              fontSize: 22, color: Colors.black),
                         ),
                 ),
               ),

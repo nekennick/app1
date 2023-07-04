701a2a86d110e00efb1e6eb28f301545536bea96
nekennick <nekennick@gmail.com>
2023-07-04 07:49:52 -0700

    sace

--- lib/logic/resultname.dart
+++ lib/logic/resultname.dart
@@ -1,10 +1,10 @@
 String getResultName(int number) {
   switch (number) {
     case 1:
-      return '"Xin chúc mừng! Tình yêu của bạn rất là đẹp đó nhé. Tình cảm của 2 bạn khá mặn nồng, khiến không ít người ngưỡng mộ và cảm phục đấy. Người ta thường nói là duyên phận ý trời mà, có tránh cũng sẽ không tránh được đâu nhá. Bạn nên đón nhận tình cảm thật của mình đi và đừng có trốn tránh nữa nhé."';
+      return '"Tình cảm của 2 bạn khá mặn nồng, khiến không ít người ngưỡng mộ và cảm phục đấy. Người ta thường nói là duyên phận ý trời mà, có tránh cũng sẽ không tránh được đâu nhá. Bạn nên đón nhận tình cảm thật của mình đi và đừng có trốn tránh nữa nhé."';
 
     case 2:
-      return '"Số 2 nói với bạn rằng trái tim mách bảo tình yêu nhưng con tim lại không thể làm chủ được lý trí của mình. Mặc dù yêu nhau nồng thắm nhưng các bạn lại không có lý trí, không thể vượt qua được những định kiến, sự cấm cản của gia đình."';
+      return '"Kết quả từ tên của hai bạn cho thấy, trái tim mách bảo tình yêu nhưng con tim lại không thể làm chủ được lý trí của mình. Mặc dù yêu nhau nồng thắm nhưng các bạn lại không có lý trí, không thể vượt qua được những định kiến, sự cấm cản của gia đình."';
 
     case 3:
       return '"Các bạn có một tình bạn rất là đẹp và trong sáng, thuần khiết một tình yêu mà bao nhiêu người phải ngưỡng mộ. Xin chúc mừng nhé."';
@@ -13,7 +13,7 @@ String getResultName(int number) {
       return '"Hai bạn cũng có một chuyện tình vô cùng đẹp mà nhiều người ao ước lắm đó. Hãy thầm cảm ơn ông tơ bà nguyệt đã se duyên đi nào."';
 
     case 5:
-      return '"Có vẻ như 2 bạn chỉ nên dừng lại ở mức độ tình bạn thôi. Đừng quá buồn nhé bởi tình yêu đích thực của cuộc đời bạn đang trên đường tìm đến với bạn đấy. Có thể do tắc đường nên sẽ đến chậm một chút thôi mà."';
+      return '"Người ấy luôn nhắc đến tên bạn và nghĩ tới bạn bất kỳ ở đâu. Bạn đã sẵn sàng cho cuộc tình thú vị này chưa? Hai người đã có một điểm khởi đầu khá tốt đẹp. Còn chần chừ gì nữa, hãy bắt đầu cho một tình yêu đẹp và lãng mạn."';
 
     case 6:
       return '"Có vẻ 2 bạn là định mệnh của đời nhau đúng không? Yêu nhau từ cái nhìn đầu tiên à? Xin chúc mừng nhé, 2 bạn rất hợp nhau đấy."';
